fc7da018479e98b20261dab00f638ed2cdd68b08
Justin Tendeck <justin.tendeck@gmail.com>
2019-09-14 20:23:10 -0700

    added alert system

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -41,14 +41,12 @@ function saveUserData(role, email, password) {
         //Sign in
         auth.createUserWithEmailAndPassword(email,password).then(function(firebaseUser) {
             auth.onAuthStateChanged(function(user) {
-                console.log(user.uid);
                 writeUserData(user.uid, email, role)
             });
         });
 }
 
 function writeUserData(userId, email, role) {
-    console.log("user" + userId)
     firebase.database().ref('users/' + userId).set({
       userId,
       email,
@@ -82,8 +80,3 @@ if (btnLogOut) {
         firebase.auth().signOut();
     });
 }
-
-console.log(window.location.href);
-
-// Realtime Listener
-console.log("Firebase loaded");
